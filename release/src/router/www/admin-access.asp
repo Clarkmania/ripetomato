Clarkmania/ripetomato

commit 18d27b51edc98672e7dc61364d06997ccbc13dfa

Disable online themes for now.

release/src/router/www/admin-access.asp
@@ -222,7 +222,7 @@ No part of this file may be used without permission.
                     { title: 'Allow Wireless Access', name: 'f_http_wireless', type: 'checkbox', value:  nvram.web_wl_filter == 0 },
                     { title: '<h5>Interface Settings</h5>', rid: 'row_sslcert' },
                     { title: 'Interface Theme', name: 'web_css', type: 'select', help: 'With RipeTomato you get very few built in skins, others are available on AdvancedTomato Themes Directory. This way we can save space on router for more important functions.',
-                     options: [['tomato','Default'], ['css/metro','Metro'],['ext/custom','Custom (ext/custom.css)'], ['online', 'On-line from TTD']], value: nvram.web_css },
+                     options: [['tomato','Default'], ['css/metro','Metro'],['ext/custom','Custom (ext/custom.css)'], value: nvram.web_css },
                     { title: 'TTD ID#', indent: 2, name: 'ttb_css', type: 'text', maxlen: 25, size: 30, value: nvram.ttb_css, suffix: ' Theme ID# from <a href="http://clarkmania.github.io/ripetomato/themes/" target="_blank"><u><i>TTD themes gallery</i></u></a>' },
                     { title: 'Interface Width', help: "This preference allows you to change the way web interface adapts to your browser screen.", size: 35, 
                      name: 'at_width', type: 'select', options: [['fluid','Fluid (Full Browser Size)'],['1024','Fixed: 1024px'], ['1280','Fixed: 1280px'], ['1680','Fixed: 1680px']], value: nvram.at_width },

commit a6ee7f4580b0a461c8d5417065dd5790d2cb8508

Fix admin access page, broken in commits 18d27b51 aca71b00

release/src/router/www/admin-access.asp
@@ -42,7 +42,7 @@ No part of this file may be used without permission.
 			var o = (E("_web_css").value == "online");
 			var p = nvram.ttb_css;
 
-			elem.display(PR("_ttb_css"), o);
+			//elem.display(PR("_ttb_css"), o);
 
 			try {
 				a = E("_web_css").value;
@@ -231,7 +231,7 @@ No part of this file may be used without permission.
 						{ title: 'Allow Wireless Access', name: 'f_http_wireless', type: 'checkbox', value:  nvram.web_wl_filter == 0 },
 						{ title: '<h5>Interface Settings</h5>' },
 						{ title: 'Interface Theme', name: 'web_css', type: 'select', 
-							options: [['tomato','Default'], ['ext/custom','Custom (ext/custom.css)'], value: nvram.web_css },
+							options: [['tomato','Default'], ['ext/custom','Custom (ext/custom.css)']], value: nvram.web_css },
 /*
 						{ title: 'ATTD ID#', indent: 2, name: 'ttb_css', type: 'text', maxlen: 25, size: 30, value: nvram.ttb_css, suffix: 'Theme ID# from <a href="http://at.prahec.com/themes/" target="_blank"><u><i>ATTD themes gallery</i></u></a>' },
 */